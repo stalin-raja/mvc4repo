ea5b7ae5510ee21564140671bbbe5de08eba3b9e
stalin-raja <stalinr2007@gmail.com>
2015-09-22 20:41:33 +0530

    XTRA Controller Added

---
+++ MVC4/Controllers/XtraController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC4.Controllers
+{
+    public class XtraController : Controller
+    {
+        //
+        // GET: /Xtra/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
